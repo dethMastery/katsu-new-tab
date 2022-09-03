fd07446d7598efbd465500a000fb3a4c466f0275
dethMastery <owner@detzz.in.th>
2022-09-03 16:22:12 +0700

    added: katsu's Released

--- static/scripts/RSSFetch.js
+++ static/scripts/RSSFetch.js
@@ -2,7 +2,6 @@ let parser = new RSSParser()
 
 parser.parseURL('https://blog.suphakit.net/atom.xml', function (err, feed) {
   if (err) throw err
-  console.log(feed.title);
   let blog = document.querySelector('#katsuBlog')
 
   let header = document.createElement("h2")
@@ -19,9 +18,9 @@ parser.parseURL('https://blog.suphakit.net/atom.xml', function (err, feed) {
     let link = document.createElement('a')
     link.setAttribute('href', feed.items[i].link)
     let list = document.createElement('li')
-    list.innerText = feed.items[i].title
-    link.appendChild(list)
-    hList.appendChild(link)
+    link.innerText = feed.items[i].title
+    list.appendChild(link)
+    hList.appendChild(list)
   }
 
   blog.appendChild(document.createElement('br'))
@@ -34,4 +33,40 @@ parser.parseURL('https://blog.suphakit.net/atom.xml', function (err, feed) {
   see_more.innerText = 'See More >>'
   link_container.appendChild(see_more)
   blog.appendChild(link_container)
+})
+
+parser.parseURL('https://katsuragi.detzz.in.th/api/rss/index.php', function(err, feed) {
+  if (err) throw err
+
+  let content = document.querySelector("#katsu-wav")
+
+  let head = document.createElement('h2')
+  let hList = document.createElement('ul')
+  head.innerText = 'katsuragi.wav Released'
+
+  content.appendChild(head)
+  content.appendChild(document.createElement('br'))
+  content.appendChild(hList)
+  
+  for (let i=0; i<4; i++) {
+    let link = document.createElement('a')
+    let list = document.createElement('li')
+
+    link.setAttribute('href', feed.items[i].link)
+    link.innerText = feed.items[i].author + ' - ' + feed.items[i].title
+    list.appendChild(link)
+    hList.appendChild(list)
+  }
+
+  content.appendChild(document.createElement('br'))
+
+  let link_container = document.createElement('div')
+  let see_more = document.createElement('a')
+
+  link_container.setAttribute('class', 'linkContainer')
+  see_more.setAttribute('href', 'https://katsuragi.detzz.in.th/#released')
+  see_more.setAttribute('class', 'seeMore')
+  see_more.innerText = 'See More >>'
+  link_container.appendChild(see_more)
+  content.appendChild(link_container)
 })
